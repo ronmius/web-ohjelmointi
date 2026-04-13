85cc955fff2b526a8fb240e0abc8e75cf91dde3e
Roni Somervuori <roni.somervuori@kukkula.local>
2026-04-13 12:13:44 +0300

    js_muutujat

---
+++ portfolio/muutujia.js
@@ -0,0 +1,17 @@
+console.log("lasketaan")
+a = 3
+b=5
+c=7
+d=15
+function summa (){
+    alert(a+b)
+}
+function vahennys(){
+    alert(b-c)
+}
+function kerto (){
+    alert (c*d)
+}
+function jako (){
+    alert(d/a)
+}
