c8a41a292d3347285f6f05c15029a4b9002d20f0
Roni Somervuori <roni.somervuori@kukkula.local>
2026-04-13 14:43:37 +0300

    js_laskuri

--- portfolio/muutujia.js
+++ portfolio/muutujia.js
@@ -7,11 +7,39 @@ function summa (){
     alert(a+b)
 }
 function vahennys(){
-    alert(b-c)
+    alert(a-b)
 }
 function kerto (){
-    alert (c*d)
+    alert (a*b)
 }
 function jako (){
-    alert(d/a)
+    alert(a/b)
+}
+function laskin_summa (){
+    ekluku = Number(document.getElementById("ekluku").value);
+    tokluku = Number(document.getElementById("tokluku").value);
+    a = ekluku
+    b = tokluku
+    summa ()
+}
+function laskin_vähennys (){
+    ekluku = Number(document.getElementById("ekluku").value);
+    tokluku = Number(document.getElementById("tokluku").value);
+    a = ekluku
+    b = tokluku
+    vahennys ()
+}
+function laskin_kerto(){
+    ekluku = Number(document.getElementById("ekluku").value);
+    tokluku = Number(document.getElementById("tokluku").value);
+    a = ekluku
+    b = tokluku
+    kerto ()
+}
+function laskin_jako (){
+    ekluku = Number(document.getElementById("ekluku").value);
+    tokluku = Number(document.getElementById("tokluku").value);
+    a = ekluku
+    b = tokluku
+    jako ()
 }
