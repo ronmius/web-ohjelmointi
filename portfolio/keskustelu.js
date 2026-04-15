84837c10ffbf394e73cade534a2eb0c84783322e
Roni Somervuori <roni.somervuori@kukkula.local>
2026-04-15 14:00:07 +0300

    keskustelua

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -2,3 +2,15 @@ console.log("keskustelaan")
 function hello (){
     alert("hello World!")
 }
+function send (e){
+    e.preventDefault();
+    typefield =  e.target.form.fav_language.value
+    usagefield = e.target.form.usage.value
+    emailField = e.target.form.emailField.value
+    birthdayField = e.target.form.birthdayField.value
+    bodyField = e.target.form.bodyField.value
+    console.log(birthdayField,emailField,typefield,usagefield,bodyField)
+    
+    const diolg =document.getElementById("lisaa")
+    diolg.close();
+}
