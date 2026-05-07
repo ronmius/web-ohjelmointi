31d698227c08620f5534de331607791cd2f6b14e
Roni Somervuori <roni.somervuori@kukkula.local>
2026-05-07 15:51:40 +0300

    henkilötiedot

--- portfolio/henkilotiedot.js
+++ portfolio/henkilotiedot.js
@@ -8,7 +8,7 @@ let henkilöt = [
     {
         name: "Semir Sikäläinen",
         age: 18,
-        job: "opiskelija",
+        job: "Opiskelija",
         driversLicense: true
     },
     {
@@ -31,12 +31,23 @@ let henkilöt = [
     }
 ]
 
-
-function LuoRivit (){
+LuoRivit (e)
+function LuoRivit (e){
+    e.preventDefault()
     reveat = document.getElementById("rivit")
     console.log(reveat)
     reveat.clear
-    henkilöt.forEach(hlö => {
+    // lisättävän henkilön tiedot otetaan näillä
+    var lisnim = document.getElementById("Nimi").value;
+    var lisika = Number(document.getElementById("Ikä").value);
+    var listyö = document.getElementById("Työ").value;
+    var lisajo = Boolean(document.getElementById("Ajokortti"));
+    if (lisika <0){alert('ikä pitää olla suurempi kuin nolla')};
+    
+    console.log(lisajo);
+    henkilöt.push({name:lisnim,age:lisika,job:listyö,driversLicense:lisajo});
+    console.log(henkilöt)
+    henkilöt.forEach(hlö => {//tämän avulla tehdään oikea määrä rivejä ja muutujia
         var rivi = document.createElement("tr")
         var nimi = document.createElement("td")
         var ika =document.createElement("td")
@@ -44,12 +55,22 @@ function LuoRivit (){
         var ajaa = document.createElement("td")
         
         nimi.innerHTML = hlö.name
-        ika.innerHTML = hlö.age
-        työ.innerHTML = hlö.job
+        if (hlö.age >17){//jos henkilö on yli 18 tulee sen viereen kalja
+            ika.innerHTML = `${hlö.age}🍺`
+        }
+        else
+            {ika.innerHTML = hlö.age}
+        if (hlö.job == "Opiskelija"){
+            työ.innerHTML= `${hlö.job} 🎓`//jos henkilö on opiskelija lakki tulee sen viereen
+        }
+        else{työ.innerHTML = hlö.job}
         ajaa.innerHTML = hlö.driversLicense
         rivi.append(nimi,ika,työ,ajaa)
-
         reveat.append(rivi)
     });
 }
-LuoRivit ()
+function sulje (e){
+    e.preventDefault();
+    const diolg =document.getElementById("lisaa");
+    diolg.close()
+}
